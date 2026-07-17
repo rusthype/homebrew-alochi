c875949ca8125a427ac30937db8ec60b94b7e06d
Abdulhamid Rustamov <rustamovabdulhamid976@gmail.com>
2026-07-17 14:29:02 +0500

    fix: correct cask to use DMG + actual .app bundle name

--- Casks/alochi-monitoring.rb
+++ Casks/alochi-monitoring.rb
@@ -1,11 +1,11 @@
 cask "alochi-monitoring" do
-  version "1.0.0"
+  version "1.0.50"
   sha256 "0000000000000000000000000000000000000000000000000000000000000"
 
-  url "https://github.com/rusthype/alochi-monitoring/releases/download/v#{version}/alochi-monitoring-macos.zip"
+  url "https://github.com/rusthype/alochi-monitoring/releases/download/v#{version}/alochi-monitoring.dmg"
   name "Alochi Monitoring"
   desc "Alochi maktablar uchun monitoring tizimi"
   homepage "https://alochi.org"
 
-  app "Alochi Monitoring.app"
+  app "alochi_monitoring.app"
 end
