512c940392f7033a2d42e468f40d44d479bd2846
Abdulhamid Rustamov <rustamovabdulhamid976@gmail.com>
2026-09-09 20:36:45 +0500

    bump alochi-monitoring to v1.0.124

--- Casks/alochi-monitoring.rb
+++ Casks/alochi-monitoring.rb
@@ -1,8 +1,8 @@
 cask "alochi-monitoring" do
-  version "1.0.123"
-  sha256 "b73371672fd814733ca21f78bf2aa70cccabdb30641ffca8bb26550b3e5cbc2a"
+  version "1.0.124"
+  sha256 "36a8d98c6587c4fcfd08238f5d801788e9a7867caac770ac0a9d8136479eca8f"
 
-  url "https://github.com/rusthype/alochi-monitoring/releases/download/v1.0.123/alochi-monitoring.dmg"
+  url "https://github.com/rusthype/alochi-monitoring/releases/download/v1.0.124/alochi-monitoring.dmg"
   name "Alochi Monitoring"
   desc "Alochi maktablar uchun monitoring tizimi"
   homepage "https://alochi.org"
